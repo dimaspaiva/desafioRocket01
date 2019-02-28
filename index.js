9ce02db34e26d61d6f236670912a0d116eebb352
GP-ADM-TI-Dimas Paiva <dimasalpaiva@gmail.com>
2019-02-28 11:04:10 -0300

    Ft: criação de rotas e views

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 
 const app = express()
 
+app.set('view engine', 'njk')
+
+nunjucks.configure('views', {
+  // Inicia o live reaload
+  watch: true,
+  // Informa o servidor utilizado
+  express: app,
+  // Alterar nome de arquivos
+  autoescape: true
+})
+
 app.get('/', (req, res) => {
-  res.send('Server funcionando')
+  res.render('index')
+})
+
+app.get('/maioridade', (req, res) => {
+  res.render('maioridade')
+})
+
+app.get('/menoridade', (req, res) => {
+  res.render('menoridade')
 })
 
 app.listen(3000, () => {
